99fb16746473aaf27c815f15534ce149a9e482ca
eduart hajko <54068413+EduartHajko@users.noreply.github.com>
2025-09-25 15:55:52 +0200

    AI: Applied reviewer fix

--- Controllers/ApplicationsController.cs
+++ Controllers/ApplicationsController.cs
@@ -1,5 +1,1 @@
 ViewBag.Applications = await _context.Categories.ToListAsync();
-ViewBag.Applications = await _context.Categories.ToListAsync();
-ViewBag.Applications = await _context.Categories.ToListAsync();
-ViewBag.Applications = categories;
-ViewBag.Applications = await _context.SimpleUsers
