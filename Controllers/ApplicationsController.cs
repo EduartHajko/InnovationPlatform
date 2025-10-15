86c233536007255e04712ccace42c3654c048355
eduart <eduarthajko1992@gmail.com>
2025-10-15 15:55:43 +0200

    test 2

--- Controllers/ApplicationsController.cs
+++ Controllers/ApplicationsController.cs
@@ -578,7 +578,7 @@ namespace InnovationPlatform.Controllers
         // POST: Applications/ToggleExpertStatus
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> ToggleExpertStatus(int expertId)
+        public IActionResult ToggleExpertStatus(int expertId)
         {
             try
             {
@@ -587,14 +587,14 @@ namespace InnovationPlatform.Controllers
                     return Json(new { success = false, message = "Nuk keni akses për këtë veprim." });
                 }
 
-                var expert = await _context.SimpleUsers.FindAsync(expertId);
+                var expert =  _context.SimpleUsers.Find(expertId);
                 if (expert == null || expert.Role != "Expert")
                 {
                     return Json(new { success = false, message = "Eksperti nuk u gjet." });
                 }
 
                 expert.IsActive = !expert.IsActive;
-                await _context.SaveChangesAsync();
+                 _context.SaveChanges();
 
                 string status = expert.IsActive ? "aktivizua" : "çaktivizua";
                 return Json(new { success = true, message = $"Eksperti u {status} me sukses!" });
